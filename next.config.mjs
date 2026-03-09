3884d9711c970cab67a739ca673371669112635b
Raraph84 <contact@raraph.fr>
2026-03-09 20:09:43 +0100

    Update and fix env vars to allow compilation

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,6 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.4.5.6:53032/",
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || "ws://127.3.2.1:53032/api/ws",
-    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME || "Nox",
-  },
   images: {
     remotePatterns: [
       {
