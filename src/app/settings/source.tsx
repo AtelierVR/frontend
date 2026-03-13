7a7b409be2158b4968a64f0c4297c3f403b6a26c
hactazia <hactazia@gmail.com>
2026-03-13 22:06:57 +0000

    refactor: rename 'Server Logs' to 'Logs' and 'Server Configuration' to 'Configuration' in settings

--- src/app/settings/source.tsx
+++ src/app/settings/source.tsx
@@ -78,13 +78,13 @@ export const getPageTree = (currentUser: CurrentUser | null): Root => {
       },
       {
         type: 'page',
-        name: 'Server Logs',
+        name: 'Logs',
         url: '/settings/logs',
         icon: <Icon icon="material-symbols:list-alt-rounded" />,
       },
       {
         type: 'page',
-        name: 'Server Configuration',
+        name: 'Configuration',
         url: '/settings/configs',
         icon: <Icon icon="material-symbols:tune-rounded" />,
       }] as any : [])
